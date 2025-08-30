bcb595742a54cf5311c57a2a0fd2a94a3b78754f
Cesar Guillen Cuñat <91685879+cesar-guillen@users.noreply.github.com>
2025-08-30 16:32:57 +0200

    highlight even on different code block languages

--- highlight_code.rb
+++ highlight_code.rb
@@ -1,20 +1,35 @@
 # _plugins/highlight_in_code.rb
 require 'nokogiri'
 
-Jekyll::Hooks.register :documents, :post_render do |doc|
+Jekyll::Hooks.register [:documents, :pages], :post_render do |doc|
   next unless doc.output_ext == ".html"
-  
-  # Parse the HTML document
-  parsed_doc = Nokogiri::HTML.parse(doc.output)
-  
-  # Find all code blocks
-  parsed_doc.search('code').each do |code_block|
-    # Replace ==text== with highlighted spans
-    code_block.inner_html = code_block.inner_html.gsub(/==([^=]+)==/) do |match|
-      "<span class=\"code-highlight\">#{$1}</span>"
-    end
+
+  # Parse full HTML (tolerant parser, keeps DOCTYPE and headers intact)
+  parsed_doc = Nokogiri::HTML::Document.parse(doc.output) do |config|
+    config.recover 
   end
-  
-  # Update the document output
+
+  parsed_doc.css('code').each do |code_block|
+    html = code_block.inner_html.dup
+
+    # Case 1: Rouge tokenized the markers (== as <span class="o">==</span>)
+    html.gsub!(%r{
+      <span[^>]*class="[^"]*\bo\b[^"]*"[^>]*>\s*==\s*</span>
+      (.*?)
+      <span[^>]*class="[^"]*\bo\b[^"]*"[^>]*>\s*==\s*</span>
+    }mx) do
+      inner = Regexp.last_match(1)
+      %Q{<span class="code-highlight">#{inner}</span>}
+    end
+
+    # Case 2: Plain ==text== (no Rouge spans)
+    html.gsub!(/==([^=]+)==/) do
+      %Q{<span class="code-highlight">#{$1}</span>}
+    end
+
+    code_block.inner_html = html
+  end
+
+  # Write back full HTML
   doc.output = parsed_doc.to_html
 end
